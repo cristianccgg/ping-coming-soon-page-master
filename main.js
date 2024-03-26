b2fa25905967e88a785e7d9dbba53d992e457c8c
cristianccgg <cristianccggg@gmail.com>
2024-03-25 21:09:19 -0500

    Javascript and images updated

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function validateEmail(email) {
   if (regex.test(email) === true) {
     inputEmail.style.border = "1px solid hsl(223, 87%, 63%)";
     error.style.display = "none";
+    inputEmail.value = "";
   } else {
     inputEmail.style.border = "1px solid red";
     error.style.display = "block";
